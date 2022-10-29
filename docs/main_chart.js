c287fd7cd811e7a558c21ce9d9e96f12b4497c70
ejpham <ejpham1999@gmail.com>
2022-10-29 14:50:43 -0500

    added 50 states

--- docs/main_chart.js
+++ docs/main_chart.js
@@ -6,21 +6,68 @@ google.charts.setOnLoadCallback(drawRegionsMap);
 
 function drawRegionsMap() {
 	var data = google.visualization.arrayToDataTable([
-		['State', 'Popularity'],
+		['State', 'Select'],
+		['Alaska', 0],
+		['Alabama', 0],
+		['Arkansas', 0],
+		['American Samoa', 0],
+		['Arizona', 0],
+		['California', 0],
+		['Colorado', 0],
+		['Connecticut', 0],
+		['Delaware', 0],
+		['Florida', 0],
+		['Georgia', 0],
+		['Hawaii', 0],
+		['Iowa', 0],
+		['Idaho', 0],
+		['Illinois', 0],
+		['Indiana', 0],
+		['Kansas', 0],
+		['Kentucky', 0],
+		['Louisiana', 0],
+		['Massachusetts', 0],
+		['Maryland', 0],
+		['Maine', 0],
+		['Michigan', 0],
+		['Minnesota', 0],
+		['Missouri', 0],
+		['Mississippi', 0],
+		['Montana', 0],
+		['North Carolina', 0],
+		['North Dakota', 0],
+		['Nebraska', 0],
+		['New Hampshire', 0],
+		['New Jersey', 0],
+		['New Mexico', 0],
+		['Nevada', 0],
+		['New York', 0],
+		['Ohio', 0],
+		['Oklahoma', 0],
+		['Oregon', 0],
+		['Pennsylvania', 0],
+		['Puerto Rico', 0],
+		['Rhode Island', 0],
+		['South Carolina', 0],
+		['South Dakota', 0],
+		['Tennessee', 0],
 		['Texas', 100],
-		['Alabama', 80],
-		['Arizona', 60],
-		['New York', 40],
-		['California', 20]
+		['Utah', 0],
+		['Virginia', 0],
+		['Vermont', 0],
+		['Washington', 0],
+		['Wisconsin', 0],
+		['West Virginia', 0],
+		['Wyoming', 0],
 	]);
-
+	
 	var options = {
 		region: 'US',
 		displayMode: 'regions',
 		resolution: 'provinces'
 	};
-
+	
 	var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-
+	
 	chart.draw(data, options);
 }
--- docs/test.js
+++
@@ -1,7 +0,0 @@
-function duck() {
-    fetch('https://api.eia.gov/v2/petroleum/crd/crpdn/data/?api_key=vVQCezbMDFZDw6SCxKutg7fpk43er3EOXsZuEWI0&start=2021&data[]=value')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-}
-
-duck();
