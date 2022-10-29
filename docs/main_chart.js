470dab77a8973124db0f61c08208ff66bd1ef103
wafflehops <87043293+wafflehops@users.noreply.github.com>
2022-10-29 17:22:47 -0500

    fucking finally

--- docs/main_chart.js
+++ docs/main_chart.js
@@ -1,76 +1,99 @@
-google.charts.load('current', {
-	'packages':['geochart'],
+google.charts.load("current", {
+  packages: ["geochart"],
 });
 
-google.charts.setOnLoadCallback(drawRegionsMap);
+async function fetch2021() {
+  const response = await fetch(
+    "https://api.eia.gov/v2/petroleum/crd/crpdn/data/?api_key=vVQCezbMDFZDw6SCxKutg7fpk43er3EOXsZuEWI0&start=2021&data[]=value"
+  );
+  const oilProduction = await response.json();
+  return oilProduction;
+}
 
-function drawRegionsMap() {
-	var data = google.visualization.arrayToDataTable([
-		['State', 'Oil Production'],
-		['Alaska', 0],
-		['Alabama', 0],
-		['Arkansas', 0],
-		['American Samoa', 0],
-		['Arizona', 0],
-		['California', 0],
-		['Colorado', 0],
-		['Connecticut', 0],
-		['Delaware', 0],
-		['Florida', 0],
-		['Georgia', 0],
-		['Hawaii', 0],
-		['Iowa', 0],
-		['Idaho', 0],
-		['Illinois', 0],
-		['Indiana', 0],
-		['Kansas', 0],
-		['Kentucky', 0],
-		['Louisiana', 0],
-		['Massachusetts', 0],
-		['Maryland', 0],
-		['Maine', 0],
-		['Michigan', 0],
-		['Minnesota', 0],
-		['Missouri', 0],
-		['Mississippi', 0],
-		['Montana', 0],
-		['North Carolina', 0],
-		['North Dakota', 0],
-		['Nebraska', 0],
-		['New Hampshire', 0],
-		['New Jersey', 0],
-		['New Mexico', 0],
-		['Nevada', 0],
-		['New York', 0],
-		['Ohio', 0],
-		['Oklahoma', 0],
-		['Oregon', 0],
-		['Pennsylvania', 0],
-		['Puerto Rico', 0],
-		['Rhode Island', 0],
-		['South Carolina', 0],
-		['South Dakota', 0],
-		['Tennessee', 0],
-		['Texas', 100],
-		['Utah', 0],
-		['Virginia', 0],
-		['Vermont', 0],
-		['Washington', 0],
-		['Wisconsin', 0],
-		['West Virginia', 0],
-		['Wyoming', 0]
-	]);
-	
-	var options = {
-		region: 'US',
-		displayMode: 'regions',
-		resolution: 'provinces',
-		keepAspectRatio: true,
-		width: 1400,
-		height: 700,
-	};
-	
-	var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-	
-	chart.draw(data, options);
+fetch2021().then((oil) => {
+  console.log(oil["response"]["data"]);
+  google.charts.setOnLoadCallback(drawRegionsMap(oil["response"]["data"]));
+});
+
+function getOilProductionValue(data, stateNeeded) {
+  for (const area of data) {
+    const state = area["series-description"].split(" ")[0];
+    if (stateNeeded === state && area["units"] === "MBBL") {
+      return area["value"];
+    }
+  }
+  return 0;
+}
+
+function drawRegionsMap(data) {
+  var data = google.visualization.arrayToDataTable([
+    ["State", "Oil Production"],
+    ["Alaska", getOilProductionValue(data, "Alaska")],
+    ["Alabama", getOilProductionValue(data, "Alabama")],
+    ["Arkansas", getOilProductionValue(data, "Arkansas")],
+    ["American Samoa", getOilProductionValue(data, "American Samoa")],
+    ["Arizona", getOilProductionValue(data, "Arizona")],
+    ["California", getOilProductionValue(data, "California")],
+    ["Colorado", getOilProductionValue(data, "Colorado")],
+    ["Connecticut", getOilProductionValue(data, "Connecticut")],
+    ["Delaware", getOilProductionValue(data, "Delaware")],
+    ["Florida", getOilProductionValue(data, "Florida")],
+    ["Georgia", getOilProductionValue(data, "Georgia")],
+    ["Hawaii", getOilProductionValue(data, "Hawaii")],
+    ["Iowa", getOilProductionValue(data, "Iowa")],
+    ["Idaho", getOilProductionValue(data, "Idaho")],
+    ["Illinois", getOilProductionValue(data, "Illinois")],
+    ["Indiana", getOilProductionValue(data, "Indiana")],
+    ["Kansas", getOilProductionValue(data, "Kansas")],
+    ["Kentucky", getOilProductionValue(data, "Kentucky")],
+    ["Louisiana", getOilProductionValue(data, "Louisiana")],
+    ["Massachusetts", getOilProductionValue(data, "Massachusetts")],
+    ["Maryland", getOilProductionValue(data, "Maryland")],
+    ["Maine", getOilProductionValue(data, "Maine")],
+    ["Michigan", getOilProductionValue(data, "Michigan")],
+    ["Minnesota", getOilProductionValue(data, "Minnesota")],
+    ["Missouri", getOilProductionValue(data, "Missouri")],
+    ["Mississippi", getOilProductionValue(data, "Mississippi")],
+    ["Montana", getOilProductionValue(data, "Montana")],
+    ["North Carolina", getOilProductionValue(data, "North Carolina")],
+    ["North Dakota", getOilProductionValue(data, "North Dakota")],
+    ["Nebraska", getOilProductionValue(data, "Nebraska")],
+    ["New Hampshire", getOilProductionValue(data, "New Hampshire")],
+    ["New Jersey", getOilProductionValue(data, "New Jersey")],
+    ["New Mexico", getOilProductionValue(data, "New Mexico")],
+    ["Nevada", getOilProductionValue(data, "Nevada")],
+    ["New York", getOilProductionValue(data, "New York")],
+    ["Ohio", getOilProductionValue(data, "Ohio")],
+    ["Oklahoma", getOilProductionValue(data, "Oklahoma")],
+    ["Oregon", getOilProductionValue(data, "Oregon")],
+    ["Pennsylvania", getOilProductionValue(data, "Pennsylvania")],
+    ["Puerto Rico", getOilProductionValue(data, "Puerto Rico")],
+    ["Rhode Island", getOilProductionValue(data, "Rhode Island")],
+    ["South Carolina", getOilProductionValue(data, "South Carolina")],
+    ["South Dakota", getOilProductionValue(data, "South Dakota")],
+    ["Tennessee", getOilProductionValue(data, "Tennessee")],
+    ["Texas", getOilProductionValue(data, "Texas")],
+    ["Utah", getOilProductionValue(data, "Utah")],
+    ["Virginia", getOilProductionValue(data, "Virginia")],
+    ["Vermont", getOilProductionValue(data, "Vermont")],
+    ["Washington", getOilProductionValue(data, "Washington")],
+    ["Wisconsin", getOilProductionValue(data, "Wisconsin")],
+    ["West Virginia", getOilProductionValue(data, "West Virginia")],
+    ["Wyoming", getOilProductionValue(data, "Wyoming")],
+  ]);
+
+  var options = {
+    region: "US",
+    displayMode: "regions",
+    resolution: "provinces",
+    keepAspectRatio: true,
+    width: 1400,
+    height: 700,
+  };
+
+  var chart = new google.visualization.GeoChart(
+    document.getElementById("regions_div")
+  );
+
+  chart.draw(data, options);
 }
